c25c1bc97b1ef30349f5537644447b038dc31fdf
lartoe <lars.toepner@sinnerschrader.com>
2023-01-20 14:07:42 +0100

    ci: components with index.html now automatically get served through html-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,46 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const CopyPlugin = require('copy-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const chalk = require('chalk');
+
+const componentsPath = path.join(__dirname, 'src/components');
+const componentNames = fs.readdirSync(componentsPath);
+const validComponents = [];
+
+console.log(chalk.bold('Components:'));
+componentNames.forEach((component) => {
+  if (fs.existsSync(`${componentsPath}/${component}/index.html`)) {
+    console.log(chalk.bgGreen(`📄 ${component}: http://localhost:8080/${component}.html`));
+    return validComponents.push(component);
+  } else {
+    return console.log(chalk.bgRed(`😔 ${component} has no index.html`));
+  }
+});
+console.log('\n');
 
 module.exports = {
   entry: ['./src/index.ts', './src/foundation/index.scss'],
   mode: 'development',
   devtool: 'source-map',
   optimization: {
-    usedExports: true
+    usedExports: true,
   },
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.(scss|css)$/,
@@ -33,26 +50,26 @@ module.exports = {
           {
             loader: 'sass-loader',
             options: {
-              sourceMap: true
-            }
-          }
-        ]
-      }
-    ]
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
   plugins: [
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: '[name].[contenthash].css',
-      chunkFilename: '[id].css'
+      chunkFilename: '[id].css',
     }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
     new ForkTsCheckerWebpackPlugin(),
     // new CopyPlugin({
@@ -61,7 +78,14 @@ module.exports = {
     new ESLintPlugin({
       extensions: ['.ts', '.js'],
       exclude: 'node_modules',
-      context: 'src'
+      context: 'src',
+    }),
+  ].concat(
+    validComponents.map((name) => {
+      return new HtmlWebpackPlugin({
+        template: `./src/components/${name}/index.html`,
+        filename: `${name}.html`,
+      });
     })
-  ]
+  ),
 };
