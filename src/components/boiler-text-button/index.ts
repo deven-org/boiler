335b59660891b6a52fbcdac8bc62bbd4a538b978
Oliver <oliver.klee@sinnerschrader.com>
2023-02-09 11:16:02 +0100

    feat: adds stylint for lit-css

--- src/components/boiler-text-button/index.ts
+++ src/components/boiler-text-button/index.ts
@@ -1,29 +1,10 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { IconMapping, IconType } from '../../foundation/icons';
-
+import { style } from './css';
 @customElement('boiler-text-button')
 export class BoilerTextButton extends LitElement {
-  static styles = css`
-    .boiler-text-button {
-      cursor: pointer;
-      border: 0;
-      padding: 0 var(--blr-spacing-600);
-      height: 40px;
-      color: var(--blr-color-progressive-red);
-      height: 40px;
-      border-radius: 9999px;
-      font-size: 14px;
-    }
-
-    .boiler-text-button:hover {
-      background: var(--blr-color-neon-blue-100);
-    }
-
-    .boiler-text-button:active {
-      background: var(--blr-color-neon-green-100);
-    }
-  `;
+  static styles = style;
 
   @property() label = 'Button Label';
   @property() onClick: HTMLButtonElement['onclick'];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 import './components/boiler-text-button/index.ts';
+
+export { BoilerTextButton } from './components/boiler-text-button';
+export type { IconType } from './foundation/icons/index';
+export { IconMapping } from './foundation/icons/index';
