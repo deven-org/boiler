4ef9b33aa74a697ba94fcdd2c436b19df557985e
lartoe <lars.toepner@sinnerschrader.com>
2023-03-16 08:17:24 +0100

    feat: adds react file to icon-link

---
+++ src/components/icon-link/indexReact.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createComponent, EventName } from '@lit-labs/react';
+
+import { BlrIconLink } from '.';
+
+export const BlrIconLinkReact = createComponent({
+  tagName: 'blr-icon-link',
+  elementClass: BlrIconLink,
+  react: React,
+  events: {
+    onClick: 'pointerdown' as EventName<PointerEvent>,
+  },
+});
