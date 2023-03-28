28eb48fcb159f82a9a272a064aa208955ab599e1
Oliver <oliver.klee@sinnerschrader.com>
2023-03-28 11:41:32 +0200

    feat: adds custom eslint config

--- packages/ui-library/src/index.ts
+++ packages/ui-library/src/index.ts
@@ -1,5 +1,5 @@
 import './components/text-button/index.ts';
 
 export { BlrTextButton } from './components/text-button';
-export type { IconType } from './foundation/icons/index';
-export { IconMapping } from './foundation/icons/index';
+export type { IconType } from '@boiler/icons/index';
+export { IconMapping } from '@boiler/icons/index';
