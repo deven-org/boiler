5768c8652bfedb12ccc31cbaeda7ad5c5ba6e6ae
Oliver <oliver.klee@sinnerschrader.com>
2023-04-02 18:20:22 +0200

    feat(tokens): changes how tokens are generated
    removes the need of having a normalize task beforehand

---
+++ packages/ui-library/src/foundation/_tokens-generated/__component-tokens.generated.js
@@ -0,0 +1,71 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 02 Apr 2023 16:13:49 GMT
+ */
+
+export const componentTokens = {
+  Action: {
+    TextButton: {
+      XS: { Padding: '0.25rem 12px', Gap: '4px' },
+      SM: { Padding: '0.188rem 12px', Gap: '4px' },
+      MD: { Padding: '0.438rem 16px', Gap: '4px' },
+      LG: { Padding: '0.5rem 24px', Gap: '4px' },
+      XL: { Padding: '1rem 40px', Gap: '4px' },
+    },
+    IconButton: {
+      XS: { Padding: '0.375rem', Gap: '0px' },
+      SM: { Padding: '0.375rem', Gap: '0px' },
+      MD: { Padding: '0.5rem', Gap: '0px' },
+      LG: { Padding: '0.5rem', Gap: '0px' },
+      XL: { Padding: '0.75rem', Gap: '0px' },
+    },
+    IconDropdown: {
+      XS: { Padding: '0.375rem 0.063rem 0.375rem 0.188rem', Gap: '0px' },
+      SM: { Padding: '0.375rem 0.063rem 0.375rem 0.188rem', Gap: '0px' },
+      MD: { Padding: '0.5rem 0.063rem 0.5rem 0.188rem', Gap: '0px' },
+      LG: { Padding: '0.5rem 0rem 0.5rem 0.125rem', Gap: '-0.125rem' },
+      XL: { Padding: '0.75rem 0rem 0.75rem 0.25rem', Gap: '-0.25rem' },
+    },
+  },
+  UI: { Icon: { XXS: '0.5rem', XS: '0.75rem', SM: '1rem', MD: '1.5rem', LG: '2rem', XL: '3rem' } },
+  Feedback: {
+    Loader: {
+      SM: {
+        Background: {
+          Default: { color: 'hsla(220, 85%, 60%, 0.2)', width: '2px', style: 'solid' },
+          Inverted: { color: 'hsla(220, 85%, 99%, 0.2)', width: '2px', style: 'solid' },
+        },
+        Foreground: {
+          Default: { color: 'hsla(220, 85%, 40%, 1)', width: '2px', style: 'solid' },
+          Inverted: { color: 'hsla(220, 85%, 99%, 1)', width: '2px', style: 'solid' },
+        },
+        Sizing: '1rem',
+        Padding: '0.063rem',
+      },
+      MD: {
+        Background: {
+          Default: { color: 'hsla(220, 85%, 60%, 0.2)', width: '2px', style: 'solid' },
+          Inverted: { color: 'hsla(220, 85%, 99%, 0.2)', width: '2px', style: 'solid' },
+        },
+        Foreground: {
+          Default: { color: 'hsla(220, 85%, 40%, 1)', width: '2px', style: 'solid' },
+          Inverted: { color: 'hsla(220, 85%, 99%, 1)', width: '2px', style: 'solid' },
+        },
+        Sizing: '1.5rem',
+        Padding: '0.125rem',
+      },
+      LG: {
+        Background: {
+          Default: { color: 'hsla(220, 85%, 60%, 0.2)', width: '3px', style: 'solid' },
+          Inverted: { color: 'hsla(220, 85%, 99%, 0.2)', width: '3px', style: 'solid' },
+        },
+        Foreground: {
+          Default: { color: 'hsla(220, 85%, 40%, 1)', width: '3px', style: 'solid' },
+          Inverted: { color: 'hsla(220, 85%, 99%, 1)', width: '3px', style: 'solid' },
+        },
+        Sizing: '2rem',
+        Padding: '0.188rem',
+      },
+    },
+  },
+};
