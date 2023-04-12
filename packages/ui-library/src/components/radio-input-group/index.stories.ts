9ccd41495e55b678eb70d4ddf32649b414756951
David Kennedy <david.a.kennedy@accenture.com>
2023-04-12 14:43:33 +0200

    feat(ui-library): refactoring radio input component to be a part of the 'radio input group'

---
+++ packages/ui-library/src/components/radio-input-group/index.stories.ts
@@ -0,0 +1,60 @@
+import { html } from 'lit-html';
+import { BlrRadioInputGroup as BlrRadioInputGroupClass } from './index';
+import { Sizes } from '../../globals/constants';
+import { action } from '@storybook/addon-actions';
+import './index';
+
+export default {
+  title: 'BlrRadioInputGroup',
+  argTypes: {
+    size: {
+      options: Sizes,
+      control: { type: 'select' },
+    },
+    options: { control: 'array' },
+  },
+};
+
+export const BlrRadioInputGroup = ({
+  textInputId,
+  label,
+  value,
+  disabled,
+  size,
+  required,
+  errorMessage,
+  hasError,
+  options,
+}: BlrRadioInputGroupClass) =>
+  html`
+    <blr-radio-input-group
+      .textInputId=${textInputId}
+      .label=${label}
+      .disabled=${disabled}
+      .size=${size}
+      .required=${required}
+      .onChange=${action('onChange')}
+      .onBlur=${action('onBlur')}
+      .onFocus=${action('onFocus')}
+      .errorMessage=${errorMessage}
+      .hasError=${hasError}
+      .options=${options}
+      class="example-layout-class"
+    ></blr-radio-input>
+  `;
+
+BlrRadioInputGroup.storyName = 'BlrRadioInputGroup';
+
+BlrRadioInputGroup.args = {
+  label: 'Radio Input',
+  disabled: false,
+  required: false,
+  size: 'md',
+  errorMessage: 'This is error message',
+  hasError: false,
+  options: [
+    { label: 'Option 1', value: 'option1' },
+    { label: 'Option 2', value: 'option2' },
+    { label: 'Option 3', value: 'option3' },
+  ],
+};
