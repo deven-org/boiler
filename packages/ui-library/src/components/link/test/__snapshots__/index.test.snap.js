1e9703952ab46f32e4bf2efe85d1d5913f8c59bc
JpunkWpunkt <jessica.wittig@amay3vqvhtfql.fritz.box>
2023-04-20 17:01:02 +0200

    feat(ui-library): added link component

---
+++ packages/ui-library/src/components/link/test/__snapshots__/index.test.snap.js
@@ -0,0 +1,77 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots['blr-link renders correctly'] = `<button
+  class="blr-semantic-action blr-text-button md primary"
+  disabled=""
+  id="id"
+>
+  <blr-icon
+    aria-hidden=""
+    icon="blrChevronDown"
+    size="md"
+  >
+  </blr-icon>
+  <span>
+    Label
+  </span>
+  <blr-icon
+    aria-hidden=""
+    icon="blrChevronDown"
+    size="md"
+  >
+  </blr-icon>
+</button>
+`;
+/* end snapshot blr-text-button renders correctly */
+snapshots['blr-link renders correctly'] = `<a
+  aria-label=""
+  class="blr-link blr-semantic-action blr-text-button md primary"
+  disabled=""
+  href=""
+  rel="nofollow"
+  target="_self"
+  variant="inverted"
+>
+  <blr-icon
+    aria-hidden=""
+    icon="blrChevronDown"
+    size="md"
+  >
+  </blr-icon>
+  <span>
+    Label
+  </span>
+  <blr-icon
+    aria-hidden=""
+    icon="blrChevronDown"
+    size="md"
+  >
+  </blr-icon>
+</a>
+`;
+/* end snapshot blr-link renders correctly */
+snapshots['blr-link renders correctly'] = `<a
+  class="blr-link blr-semantic-action blr-text-button md primary"
+  disabled=""
+  rel="nofollow"
+  target="_self"
+>
+  <blr-icon
+    aria-hidden=""
+    icon="blrChevronDown"
+    size="md"
+  >
+  </blr-icon>
+  <span>
+    Label
+  </span>
+  <blr-icon
+    aria-hidden=""
+    icon="blrChevronDown"
+    size="md"
+  >
+  </blr-icon>
+</a>
+`;
+/* end snapshot blr-link renders correctly */
