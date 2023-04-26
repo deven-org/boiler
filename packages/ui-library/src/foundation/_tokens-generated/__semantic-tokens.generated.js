d40fd1f95dcdf052004a5b516b2ab9d605402a7e
m-nti <117906965+m-nti@users.noreply.github.com>
2023-04-26 10:37:51 +0200

    Revert "Merge branch 'design/19-apr-001' of https://github.com/deven-org/BO1LER-Kitchen into design/19-apr-001"
    This reverts commit 72ce507e1c05c33c33ea9914a03a691fcd9740c3, reversing
    changes made to 5097385f4de44d2ed03aadd46cd1da34597a41ef.

--- packages/ui-library/src/foundation/_tokens-generated/__semantic-tokens.generated.js
+++ packages/ui-library/src/foundation/_tokens-generated/__semantic-tokens.generated.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 26 Apr 2023 08:24:39 GMT
+ * Generated on Wed, 26 Apr 2023 08:16:02 GMT
  */
 
 export const semanticTokens = {
@@ -369,11 +369,9 @@ export const semanticTokens = {
       },
     },
     InputIcon: {
-      Rest: 'hsla(220, 10%, 70%,1) ',
       Hover: 'hsla(220, 10%, 60%,1) ',
       Focus: 'hsla(220, 10%, 50%,1) ',
       Pressed: 'hsla(220, 10%, 60%,1) ',
-      Disabled: 'hsla(220, 10%, 70%,1) ',
       Error: 'hsla(350, 80%, 50%,1) ',
     },
   },
