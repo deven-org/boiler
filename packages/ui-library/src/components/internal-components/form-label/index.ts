c5077a24f208f0e951795ce8960b9730058b263e
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-05-19 14:20:23 +0200

    fix: dont change attributes but rather with states

--- packages/ui-library/src/components/internal-components/form-label/index.ts
+++ packages/ui-library/src/components/internal-components/form-label/index.ts
@@ -1,21 +1,18 @@
 import { html } from 'lit';
 import { InputSizesType } from '../../../globals/types';
+import { ifNotEmptyString } from '../../../utils/if-not-empty-string';
 
 type FormLabelType = {
   labelText: string;
+  forInputId?: string;
   labelAppendix?: string;
   labelSize: InputSizesType;
   forValue: string;
 };
 
-export const BlrFormLabel = ({
-  labelText,
-  labelAppendix: additionalInfo,
-  labelSize: size,
-  forValue,
-}: FormLabelType) => {
+export const BlrFormLabel = ({ labelText, forValue, labelSize, labelAppendix: additionalInfo }: FormLabelType) => {
   return html`
-    <label class="blr-form-label ${size}" for=${forValue}>
+    <label class="blr-form-label" for="${ifNotEmptyString(forValue)}">
       ${labelText}
       <span class="blr-form-label-appendix ${size}">${additionalInfo}</span>
     </label>
