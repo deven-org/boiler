33b67ddf9741c87d790aa8fc4fd0d46f0f751a6b
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-06-06 13:17:49 +0200

    fix(ui-library): refactored regex to use sizes array

--- packages/ui-library/src/utils/calculate-icon-name.ts
+++ packages/ui-library/src/utils/calculate-icon-name.ts
@@ -1,7 +1,11 @@
 import { IconKeys, IconType } from '@boiler/icons';
 import { SizesType } from '../globals/types';
+import { Sizes } from '../globals/constants';
 
-const capturingRegex = /(?<size>Xxs|Xs|Sm|Md|Lg|Xl)/;
+// 'Xxs|Xs|Sm|Md|Lg|Xl' but from same source as the type def
+const keyws = Sizes.map((a) => a.charAt(0).toUpperCase() + a.substring(1)).join('|');
+// /(?<size>Xxs|Xs|Sm|Md|Lg|Xl)/;
+const capturingRegex = new RegExp('(?<size>' + keyws + ')');
 
 const hasIconSize = (icon: string) => IconKeys.includes(icon);
 
