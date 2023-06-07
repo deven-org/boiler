6721d59c690afc08c370d689e200a8e933752122
Andelka <andelka.dakic@accenture.com>
2023-06-07 14:14:28 +0200

    fix(ui-library): fix after rebasing

--- packages/ui-library/src/components/internal-components/form-label/index.ts
+++ packages/ui-library/src/components/internal-components/form-label/index.ts
@@ -3,15 +3,19 @@ import { InputSizesType } from '../../../globals/types';
 
 type FormLabelType = {
   labelText: string;
-  forInputId?: string;
   labelAppendix?: string;
   labelSize: InputSizesType;
   forValue: string;
 };
 
-export const BlrFormLabel = ({ labelText, forValue, labelSize, labelAppendix: additionalInfo }: FormLabelType) => {
+export const BlrFormLabel = ({
+  labelText,
+  labelAppendix: additionalInfo,
+  labelSize: size,
+  forValue,
+}: FormLabelType) => {
   return html`
-    <label class="blr-form-label" for="${forValue || nothing}">
+    <label class="blr-form-label ${size}" for=${forValue}>
       ${labelText}
       <span class="blr-form-label-appendix ${size}">${additionalInfo}</span>
     </label>
