7554da34cc6f51253b9c3ce887798fef6e018d1b
Christian Hoffmann <112889877+ChristianHoffmannS2@users.noreply.github.com>
2023-06-16 11:39:19 +0200

    fix(ui-library): some cleanups (#216)

---
+++ packages/ui-library/src/utils/capture-size-regex.ts
@@ -0,0 +1,6 @@
+import { Sizes } from '../globals/constants';
+
+// 'Xxs|Xs|Sm|Md|Lg|Xl' but from same source as the type def
+const keyws = Sizes.map((a) => a.charAt(0).toUpperCase() + a.substring(1)).join('|');
+// /(?<size>Xxs|Xs|Sm|Md|Lg|Xl)/;
+export const capturingRegex = new RegExp('(?<size>' + keyws + ')');
