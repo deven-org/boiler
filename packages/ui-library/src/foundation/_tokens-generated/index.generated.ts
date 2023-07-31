ecbd52ed4f3df09b9dc22fc96cef9828720a9638
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-07-31 11:35:14 +0200

    fix(tokens): try pure css export for theme

--- packages/ui-library/src/foundation/_tokens-generated/index.generated.ts
+++
@@ -1,9 +0,0 @@
-import { wrapValuesWithCss } from '../../utils/wrap-values-with-css';
-import { componentTokens } from './__component-tokens.generated.js';
-import { semanticTokens } from './__semantic-tokens.generated.js';
-
-const componentTokensWrapped = wrapValuesWithCss(componentTokens);
-const semanticTokensWrapped = wrapValuesWithCss(semanticTokens);
-
-export { componentTokensWrapped as componentTokens };
-export { semanticTokensWrapped as semanticTokens };
