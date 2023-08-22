fbb788e3da926bde3c29c28c4507967669a3e2be
JpunktWpunkt <92968754+JpunktWpunkt@users.noreply.github.com>
2023-08-22 09:10:12 +0200

    fix(ui-library): added word-break to caption-text and dispplay:inline to blr-form-element

--- packages/ui-library/src/foundation/semantic-tokens/form.css.js
+++ packages/ui-library/src/foundation/semantic-tokens/form.css.js
@@ -230,6 +230,10 @@ export const { tokenizedLight: formLight, tokenizedDark: formDark } = renderThem
         }
       }
 
+      .blr-caption-text {
+        word-break: break-all;
+      }
+
       .hint-wrapper {
         display: flex;
 
@@ -276,7 +280,6 @@ export const { tokenizedLight: formLight, tokenizedDark: formDark } = renderThem
         .hint-container {
           display: flex;
           align-items: flex-start;
-          word-break: break-all;
           margin-top: 1px;
 
           .label-wrapper {
@@ -369,7 +372,7 @@ export const { tokenizedLight: formLight, tokenizedDark: formDark } = renderThem
       }
 
       .blr-form-label {
-        display: flex;
+        display: inline;
         align-items: baseline;
         color: ${Label.Rest};
 
