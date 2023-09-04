4d9731871c2e7128a256e07d8d6d4305840ba307
Deniz Saganak <dilara.deniz.saganak@accenture.com>
2023-09-04 17:49:46 +0200

    fix(ui-library): remove buttons from storyBook

--- packages/ui-library/src/components/internal-components/divider/index.ts
+++ packages/ui-library/src/components/internal-components/divider/index.ts
@@ -1,27 +1,18 @@
 import { LitElement, html } from 'lit';
-import { classMap } from 'lit/directives/class-map.js';
 import { customElement, property } from 'lit/decorators.js';
 import { DividerVariationTypes } from '../../../globals/types';
 import { dividerLight, dividerDark } from '../../../foundation/component-tokens/ui.css';
 import { ThemeType } from '../../../foundation/_tokens-generated/index.themes';
-import { styleCustom } from './index.css';
 
 @customElement('blr-divider')
 export class BlrDivider extends LitElement {
-  static styles = [styleCustom];
-
   @property() dividerDirectionVariant?: DividerVariationTypes = 'vertical';
-  @property() addMargin = false;
+  @property({ type: Boolean }) addMargin = false;
 
   @property() theme: ThemeType = 'Light';
 
   protected render() {
     const dynamicStyles = this.theme === 'Light' ? [dividerLight] : [dividerDark];
-
-    /* const classes = classMap({
-      'blr-divider': true,
-      [`${this.dividerDirectionVariant}`]: this.dividerDirectionVariant,
-    }); */
 
     return html`<style>
         ${dynamicStyles.map((style) => style)}
