d5eb1315584f37cefef38bf629b55272146c17dd
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-10-18 19:19:04 +0200

    fix(ui-library): added generic render function and applied it to the divider

---
+++ packages/ui-library/src/utils/typesafe-generic-component-renderer.ts
@@ -0,0 +1,29 @@
+import { TemplateResult, html } from 'lit';
+
+export const genericBlrComponentRenderer = <ComponentType extends { [s: string]: unknown } | ArrayLike<unknown>>(
+  props: ComponentType,
+  useLitBindings = true
+): TemplateResult<1> => {
+  const templateFragments: string[] = [];
+  const values: unknown[] = [];
+  const entries = Object.entries(props);
+
+  entries.forEach(([key, value], index) => {
+    if (index === 0) {
+      templateFragments.push(`<blr-divider${useLitBindings ? ' .' : ' '}${key}=`);
+    } else {
+      templateFragments.push(`${useLitBindings ? ' .' : ' '}${key}=`);
+    }
+
+    values.push(value);
+  });
+  templateFragments.push(`></blr-divider>`);
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  templateFragments.raw = templateFragments;
+
+  const result = html(templateFragments as unknown as TemplateStringsArray, ...values);
+
+  return result;
+};
