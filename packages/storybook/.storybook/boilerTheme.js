3310d91d89835b913519ae75a561db6685d38a49
Barkley <barkley.taylor@accenture.com>
2023-10-27 10:19:11 +0200

    feat(ui): add icon,theme and font for sidebar

---
+++ packages/storybook/.storybook/boilerTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Boiler',
+  brandImage: require('../public/B01LER.png'),
+  brandTarget: '_self',
+  fontBase: '"SourceSansPro", sans-serif',
+});
