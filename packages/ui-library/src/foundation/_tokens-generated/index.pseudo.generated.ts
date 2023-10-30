06c06a1d10ab25fb5129f886333745ac805de04e
Christian Hoffmann <112889877+ChristianHoffmannS2@users.noreply.github.com>
2023-10-30 12:56:16 +0100

    autogenerate typescript schema for semantic/components tokens (#511)
    * feat(tokens): generate typescript schema for semantic/components
    
    * missing files to commit
    
    * css themed renderfunction is now typesafe

--- packages/ui-library/src/foundation/_tokens-generated/index.pseudo.generated.ts
+++ packages/ui-library/src/foundation/_tokens-generated/index.pseudo.generated.ts
@@ -11,15 +11,23 @@
 
   This will make it possible to switch prebuild themes on the fly on a component level
 */
+
+import { componentTokens as componentTokensType } from './componentTokensType.generated';
+import { semanticTokens as semanticTokensType } from './semanticTokensType.generated';
+
 import {
   semanticTokens as semanticTokensLight,
   componentTokens as componentTokensLight,
-} from './index.Light.generated';
+} from './index.Light.generated.js';
 
-import { semanticTokens as semanticTokensDark, componentTokens as componentTokensDark } from './index.Dark.generated';
+import {
+  semanticTokens as semanticTokensDark,
+  componentTokens as componentTokensDark,
+} from './index.Dark.generated.js';
+
 import { CSSResult } from 'lit';
 
-export function renderThemedCssStrings(renderFunc: (cT: any, sT: any) => CSSResult): {
+export function renderThemedCssStrings(renderFunc: (cT: componentTokensType, sT: semanticTokensType) => CSSResult): {
   tokenizedLight: CSSResult;
   tokenizedDark: CSSResult;
 } {
