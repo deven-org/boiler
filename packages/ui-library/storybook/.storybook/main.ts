3d0f9ab64a19b3b4c58a3ae6beac56bdda030a2f
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-11-06 10:55:35 +0100

    commits from friday

--- packages/ui-library/storybook/.storybook/main.ts
+++ packages/ui-library/storybook/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/web-components-webpack5';
 import { resolve } from 'path';
 
 const config: StorybookConfig = {
-  stories: ['../../ui-library/src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
   framework: '@storybook/web-components-webpack5',
   docs: {
@@ -12,14 +12,17 @@ const config: StorybookConfig = {
 
   webpackFinal: async (config) => {
     config.module!.rules!.push({
-      loader: 'ts-loader',
-      options: {
-        configFile: '../tsconfig.json',
-      },
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: resolve(__dirname, '../static'),
     });
 
     // Return the altered config
     return config;
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
   },
 };
 
--- packages/ui-library/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import './components/text-button/index.ts';
-export { BlrTextButton } from './components/text-button';
-export type { IconType } from '@boiler/icons/index';
-export { IconMapping } from '@boiler/icons/index';
