af092b2a6216069e693001c70ca3bf8a9e050cde
RubirajAccenture <122974571+RubirajAccenture@users.noreply.github.com>
2023-11-10 11:03:44 +0100

    semantics changes in icon button and text button (#553)
    * semantics changes in icon button and text button
    
    * nesting of classes implemented

---
+++ packages/ui-library/src/foundation/_tokens-generated/config-tokens/__component-config.generated.js
@@ -0,0 +1,53 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 30 Oct 2023 10:12:09 GMT
+ */
+
+export const componentConfig = {
+  SizeVariant: {
+    Action: {
+      TextButton: {
+        XS: { Icon: 'XXS' },
+        SM: { Icon: 'XS' },
+        MD: { Icon: 'XS' },
+        LG: { Icon: 'SM' },
+        XL: { Icon: 'SM' },
+      },
+      IconButton: {
+        XS: { Icon: 'XXS' },
+        SM: { Icon: 'XS' },
+        MD: { Icon: 'SM' },
+        LG: { Icon: 'MD' },
+        XL: { Icon: 'LG' },
+      },
+      IconDropdown: {
+        XS: { Icon: 'XXS', Chevron: 'XXS' },
+        SM: { Icon: 'XS', Chevron: 'XXS' },
+        MD: { Icon: 'SM', Chevron: 'XS' },
+        LG: { Icon: 'MD', Chevron: 'SM' },
+        XL: { Icon: 'LG', Chevron: 'MD' },
+      },
+      TextDropdown: {
+        XS: { Chevron: 'XXS' },
+        SM: { Chevron: 'XS' },
+        MD: { Chevron: 'XS' },
+        LG: { Chevron: 'SM' },
+        XL: { Chevron: 'MD' },
+      },
+      StepperButton: { SM: { Icon: 'SM' }, MD: { Icon: 'SM' }, LG: { Icon: 'MD' } },
+      Menu: { MenuItem: { Icon: 'SM' } },
+      XS: { Loader: 'SM' },
+      SM: { Loader: 'SM' },
+      MD: { Loader: 'MD' },
+      LG: { Loader: 'MD' },
+      XL: { Loader: 'LG' },
+    },
+    Forms: {
+      Checkbox: { SM: { Control: { Icon: 'SM' } }, MD: { Control: { Icon: 'SM' } }, LG: { Control: { Icon: 'MD' } } },
+      SM: { CaptionComponent: { Icon: 'SM' }, InputField: { Icon: 'SM' } },
+      MD: { CaptionComponent: { Icon: 'SM' }, InputField: { Icon: 'MD' } },
+      LG: { CaptionComponent: { Icon: 'SM' }, InputField: { Icon: 'MD' } },
+    },
+    Navigation: { TabBar: { Tab: { SM: { Icon: 'XS' }, MD: { Icon: 'SM' }, LG: { Icon: 'SM' } } } },
+  },
+};
