a8ee46911d27fe3877d70a9bb94dae12f04e7dea
Christian Hoffmann <112889877+ChristianHoffmannS2@users.noreply.github.com>
2023-11-10 17:20:01 +0100

    Button Group (#506)
    * feat(ui-library): first component with children
    
    * feat(ui-library): refactored text-button-group to button-group
    
    * fix(ui-library): namings
    
    * fix(ui-library): react wrapper
    
    * fix(ui-library): cleaner code
    
    * fix(ui-library): cleaner code

---
+++ packages/ui-library/src/components/ui/button-group/index.test.ts
@@ -0,0 +1,29 @@
+import { BlrButtonGroupType, BlrButtonGroupFunction } from './index';
+
+import { fixture, expect } from '@open-wc/testing';
+import { querySelectorDeep } from 'query-selector-shadow-dom';
+import { BlrTextButtonRenderFunction } from '../../actions/buttons/text-button';
+
+const sampleParams: BlrButtonGroupType = {
+  size: 'md',
+  alignment: 'left',
+};
+
+const testContent = BlrTextButtonRenderFunction({
+  label: 'Jaok',
+  size: 'md',
+  theme: 'Light',
+  loading: false,
+  variant: 'primary',
+  loadingStatus: 'Loading',
+});
+
+describe('blr-button-group', () => {
+  it('is rendering text button inside slot', async () => {
+    const element = await fixture(BlrButtonGroupFunction(sampleParams, testContent));
+
+    const checkbox = querySelectorDeep('blr-text-button', element.getRootNode() as HTMLElement);
+
+    expect(checkbox).to.exist;
+  });
+});
