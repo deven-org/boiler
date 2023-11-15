3266016a0b685f44627d00a1b5aff1774ff6057b
David Kennedy <david.a.kennedy@accenture.com>
2023-11-15 15:31:09 +0100

    fix(ui-library): fixing stylelint warnings

--- packages/ui-library/src/components/internal-components/form-caption-group/index.css.ts
+++ packages/ui-library/src/components/internal-components/form-caption-group/index.css.ts
@@ -1,17 +1,22 @@
-import { css } from "nested-css-to-flat/lit-css";
+import { typeSafeNestedCss } from "../../../utils/nested-typesafe-css-literals";
+
 import { semanticTokens } from "../../../foundation/_tokens-generated/__semantic-tokens.Light.generated.mjs";
 
 const { SM, MD, LG } = semanticTokens.Forms;
 
-export const formCaptionGroupStyle = css`
-.blr-form-caption-group {
-  &.sm {
-    margin: ${SM.CaptionSlot.Margin};
-  } 
-  &.md {
-    margin: ${MD.CaptionSlot.Margin};
-  } 
-  &.lg {
-    margin: ${LG.CaptionSlot.Margin};
+export const formCaptionGroupStyle = typeSafeNestedCss`
+  .blr-form-caption-group {
+
+    &.sm {
+      margin: ${SM.CaptionSlot.Margin};
+    }
+
+    &.md {
+      margin: ${MD.CaptionSlot.Margin};
+    }
+
+    &.lg {
+      margin: ${LG.CaptionSlot.Margin};
+    }
   }
-}`;
+`;
