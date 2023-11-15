239a5bfa165be7cfab4d374331e0ef64b5aa3932
David Kennedy <david.a.kennedy@accenture.com>
2023-11-15 12:21:54 +0100

    feat(ui-library): adding final unit tests for caption group

--- packages/ui-library/src/components/forms/text-input/index.test.ts
+++ packages/ui-library/src/components/forms/text-input/index.test.ts
@@ -1,7 +1,7 @@
 import { BlrTextInputRenderFunction, BlrTextInputType } from '.';
 
 import { fixture, expect } from '@open-wc/testing';
-import { querySelectorDeep } from 'query-selector-shadow-dom';
+import { querySelectorAllDeep, querySelectorDeep } from 'query-selector-shadow-dom';
 import { getRandomString } from '../../../utils/get-random.string';
 
 const sampleParams: BlrTextInputType = {
@@ -83,5 +83,28 @@ describe('blr-text-input', () => {
     const label = querySelectorDeep('blr-form-label', inputContainer?.getRootNode() as HTMLElement);
 
     expect(label).to.not.exist;
+  });
+
+  it('is shows adjacent caption components in caption group slot', async () => {
+    const element = await fixture(
+      BlrTextInputRenderFunction({
+        ...sampleParams,
+        showHint: true,
+        hintIcon: 'blrInfo',
+        hasError: true,
+        errorIcon: 'blrErrorFilled',
+      })
+    );
+
+    const formCaptions = querySelectorAllDeep('blr-form-caption', element.getRootNode() as HTMLElement);
+
+    const formCaptionHint = querySelectorDeep('.blr-form-caption', formCaptions[0] as HTMLElement);
+    const hintClassName = formCaptionHint?.className;
+
+    const formCaptionError = querySelectorDeep('.blr-form-caption', formCaptions[1] as HTMLElement);
+    const errorClassName = formCaptionError?.className;
+
+    expect(hintClassName).to.contain('hint');
+    expect(errorClassName).to.contain('error');
   });
 });
