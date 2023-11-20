fb062d65f0d858793d99f8788088218dc686dab3
JpunktWpunkt <92968754+JpunktWpunkt@users.noreply.github.com>
2023-11-20 12:45:31 +0100

    fix(storybook): added boilerTheming

---
+++ packages/storybook/.storybook/boilerTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+import B01LER from '../static/assets/public/B01LER.png'
+
+export default create({
+    base: 'dark',
+    brandTitle: 'Boiler',
+    brandImage: B01LER,
+    brandTarget: '_self',
+    fontBase: '"SourceSansPro", sans-serif',
+});
---
+++ packages/storybook/.storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import boilerTheme from './boilerTheme';
+
+addons.setConfig({
+    theme: boilerTheme,
+});
