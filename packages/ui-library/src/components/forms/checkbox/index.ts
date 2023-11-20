d35fb5eedbd7e46dc9190e6d379b9284f2310385
Christian Hoffmann <112889877+ChristianHoffmannS2@users.noreply.github.com>
2023-11-20 18:49:57 +0100

    533 rename checkbox tokens (#619)
    * refactor(tokens): Checkbox
    
    * fix(ui-library): moved css to component and some refactoring
    
    * fix(tokens): Checkbox references
    
    * fixed syntax errors
    
    * renesting and deleting of doubles
    
    * moved font family out of size blocks
    
    * broken checkbox
    
    * fix(tokens) try to trim values
    
    * commit before switch
    
    * tokens applied, but not visible checkbox there
    
    * more progress
    
    * fix: checkbox utilizes outline instead of border
    
    * fix: fixes control in error state
    
    * feat: checkbox adds colors for label in states
    
    * fix: disabled states; TODO: ReadOnly
    
    * feat(tokens): adds error colors for checkbox active
    
    * feat: adds css for checkbox selected error
    
    * readonly
    
    * implemented checkbox with real icon, radonly/disabled/check/error works
    
    ---------
    
    Co-authored-by: larserbach <larserbach@users.noreply.github.com>

--- packages/ui-library/src/components/forms/checkbox/index.ts
+++ packages/ui-library/src/components/forms/checkbox/index.ts
@@ -1,23 +1,26 @@
 import { LitElement, html, nothing } from 'lit';
-import { customElement, property, query } from 'lit/decorators.js';
+import { customElement, property, query, state } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 
 import { BlrFormLabelInline } from '../../internal-components/form-label/form-label-inline';
 import { FormSizesType } from '../../../globals/types';
 
-import { styleCustom } from './index.css';
+import { styleCustomLight, styleCustomDark } from './index.css';
 import { formDark, formLight } from '../../../foundation/semantic-tokens/form.css';
-import { checkboxDark, checkboxLight } from '../../../foundation/component-tokens/checkbox.css';
+
 import { BlrFormHintRenderFunction } from '../../internal-components/form-hint';
 import { SizelessIconType } from '@boiler/icons';
 import { ThemeType } from '../../../foundation/_tokens-generated/index.themes';
 import { genericBlrComponentRenderer } from '../../../utils/typesafe-generic-component-renderer';
+import { BlrIconRenderFunction } from '../../ui/icon';
+import { calculateIconName } from '../../../utils/calculate-icon-name';
+import { getComponentConfigToken } from '../../../utils/get-component-config-token';
 
 const TAG_NAME = 'blr-checkbox';
 
 @customElement(TAG_NAME)
 export class BlrCheckbox extends LitElement {
-  static styles = [styleCustom];
+  static styles = [];
 
   @query('input')
   protected _checkboxNode!: HTMLInputElement;
@@ -45,6 +48,18 @@ export class BlrCheckbox extends LitElement {
 
   @property() theme: ThemeType = 'Light';
 
+  @state() protected focused = false;
+
+  protected handleFocus = (event: FocusEvent) => {
+    this.focused = true;
+    this.onFocus?.(event);
+  };
+
+  protected handleBlur = (event: FocusEvent) => {
+    this.focused = false;
+    this.onBlur?.(event);
+  };
+
   protected handleChange(event: Event) {
     if (!this.disabled) {
       this.onChange?.(event);
@@ -52,27 +67,69 @@ export class BlrCheckbox extends LitElement {
   }
 
   protected render() {
-    const dynamicStyles = this.theme === 'Light' ? [formLight, checkboxLight] : [formDark, checkboxDark];
+    const dynamicStyles = this.theme === 'Light' ? [formLight, styleCustomLight] : [formDark, styleCustomDark];
 
     const classes = classMap({
       'blr-semantic-action': true,
       'blr-checkbox': true,
-      [`error`]: this.hasError || false,
-      [`disabled`]: this.disabled || false,
-      [`checked`]: this.checked || false,
-      [`readonly`]: this.readonly || false,
-      [`indeterminate`]: this.indeterminate || false,
+      'error': this.hasError || false,
+      'disabled': this.disabled || false,
+      'focus': this.focused || false,
+      'checked': this.checked || false,
+      'readonly': this.readonly || false,
+      'indeterminate': this.indeterminate || false,
       [`${this.size}`]: this.size || 'md',
     });
+
+    const labelWrapperClasses = classMap({
+      'label-wrapper': true,
+      'readonly': this.readonly || false,
+    });
+
+    const visualCheckboxClasses = classMap({
+      'visual-checkbox': true,
+      'input-control': true,
+      'disabled': this.disabled || false,
+      'focus': this.focused || false,
+      'checked': this.checked || false,
+      'readonly': this.readonly || false,
+    });
+
+    const checkerIconClasses = classMap({
+      'checker-icon': true,
+    });
+
+    const checkerIconSizeVariant = getComponentConfigToken([
+      'Checkbox',
+      this.size.toUpperCase(),
+      'Control',
+      'Icon',
+    ]).toLowerCase() as FormSizesType;
 
     return html`
       <style>
         ${dynamicStyles.map((style) => style)}
       </style>
       <div class="${classes}">
+        <label class="${visualCheckboxClasses}" for="${this.checkInputId}">
+          ${this.indeterminate
+            ? BlrIconRenderFunction({
+                icon: calculateIconName('blrMinus', checkerIconSizeVariant),
+                size: 'lg',
+                hideAria: true,
+                classMap: checkerIconClasses,
+              })
+            : BlrIconRenderFunction({
+                icon: calculateIconName('blrCheckmark', checkerIconSizeVariant),
+                size: 'lg',
+                hideAria: true,
+                classMap: checkerIconClasses,
+              })}
+        </label>
+
         <input
           type="checkbox"
-          class="input-control"
+          class="${visualCheckboxClasses}"
           id=${this.checkInputId || nothing}
           name=${this.checkInputId || nothing}
           ?disabled=${this.disabled}
@@ -81,12 +138,12 @@ export class BlrCheckbox extends LitElement {
           ?readonly=${this.readonly}
           ?hasError=${this.hasError}
           @change=${this.handleChange}
-          @focus=${this.onFocus}
-          @blur=${this.onBlur}
+          @focus=${this.handleFocus}
+          @blur=${this.handleBlur}
         />
-        <div class="label-wrapper">
+        <div class="${labelWrapperClasses}">
           ${this.hasLabel
-            ? html`${BlrFormLabelInline({ labelText: this.label, forValue: this.checkInputId, labelSize: this.size })}`
+            ? BlrFormLabelInline({ labelText: this.label, forValue: this.checkInputId, labelSize: this.size })
             : nothing}
           ${this.showHint
             ? html`
