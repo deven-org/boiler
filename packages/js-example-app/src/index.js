80c851dd938058f8cce285c26d87f0f6ec1e4d3c
David Kennedy <david.a.kennedy@accenture.com>
2023-12-01 14:56:57 +0100

    WIP: vanilla js event testing

--- packages/js-example-app/src/index.js
+++ packages/js-example-app/src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import '@boiler/ui-library/dist/index.js'
+import '@boiler/ui-library/dist/index.js';
+import {html, render} from 'lit-html';
 
 function component() {
     const element = document.createElement('div');
@@ -12,10 +13,21 @@ function component() {
   
   document.body.appendChild(component());
 
+  const sayHi = () => html`<blr-textarea .placeholder="Enter text..." .value="" .showHint=${true} .hintText="Hint" .isResizeable="both" .showCounter=${true} .maxLength=${130} @david=${() => {console.log('test')}} .warningLimitType="warningLimitInt" .warningLimitInt=${105}></blr-textarea>`;
+  render(sayHi('David'), document.querySelector('#someId'));
+
 
   const toggleButton = document.querySelector('#toggleLoadingState');
   const blrButton = document.getElementsByTagName('blr-text-button')[0];
 
+  const textarea = document.querySelector('#someId>blr-textarea');
+
+
   toggleButton.addEventListener("click", () => {
     blrButton.setAttribute('loading', 'true');
   });
+
+  textarea.addEventListener("david", () => {
+    console.log('test')
+  });
+
