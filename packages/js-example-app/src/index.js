131b872137e3c6582ca3db23ea0382bfa0fffc0e
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-12-05 14:23:39 +0100

    app build process

--- packages/js-example-app/src/index.js
+++ packages/js-example-app/src/index.js
@@ -1,33 +1,17 @@
-import _ from 'lodash';
 import '@boiler/ui-library/dist/index.js';
-import {html, render} from 'lit-html';
+import './style.scss';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const toggleButton = document.querySelector('#toggleLoadingState');
+const blrButton = document.getElementsByTagName('blr-text-button')[0];
 
-  const sayHi = () => html`<blr-textarea .placeholder="Enter text..." .value="" .showHint=${true} .hintText="Hint" .isResizeable="both" .showCounter=${true} .maxLength=${130} @david=${() => {console.log('test')}} .warningLimitType="warningLimitInt" .warningLimitInt=${105}></blr-textarea>`;
-  render(sayHi('David'), document.querySelector('#someId'));
+console.log({ toggleButton, blrButton });
 
+toggleButton.addEventListener('click', () => {
+  const currentState = blrButton.getAttribute('loading');
 
-  const toggleButton = document.querySelector('#toggleLoadingState');
-  const blrButton = document.getElementsByTagName('blr-text-button')[0];
-
-  const textarea = document.querySelector('#someId>blr-textarea');
-
-
-  toggleButton.addEventListener("click", () => {
+  if (currentState) {
+    blrButton.removeAttribute('loading');
+  } else {
     blrButton.setAttribute('loading', 'true');
-  });
-
-  textarea.addEventListener("david", () => {
-    console.log('test')
-  });
-
+  }
+});
--- packages/js-example-app/webpack.config.js
+++ packages/js-example-app/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +7,24 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'src/assets', to: '.' }],
+    }),
+  ],
 };
