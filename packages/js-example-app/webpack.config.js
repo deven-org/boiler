c52cf3f5f0049fe370963b358ff86621409caff3
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-12-11 15:53:58 +0100

    try build all script in render

--- packages/js-example-app/webpack.config.js
+++ packages/js-example-app/webpack.config.js
@@ -1,30 +1,33 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
+module.exports = function (env) {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: !!env.useCustomBasePath ? '/hs-example-app/' : '/',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [{ from: 'src/assets', to: '.' }],
+      }),
     ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: 'src/assets', to: '.' }],
-    }),
-  ],
+  };
 };
