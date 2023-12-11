ff6098f3d392279c7e78c2dd1171f421e1ff3f7c
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-12-11 16:25:02 +0100

    try build all script in render

--- packages/js-example-app/webpack.config.js
+++ packages/js-example-app/webpack.config.js
@@ -1,33 +1,24 @@
 const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = function (env) {
-  return {
-    entry: './src/index.js',
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: !!env.useCustomBasePath ? '/hs-example-app/' : '/',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            'style-loader',
-            // Translates CSS into CommonJS
-            'css-loader',
-            // Compiles Sass to CSS
-            'sass-loader',
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [{ from: 'src/assets', to: '.' }],
-      }),
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
     ],
-  };
+  },
 };
