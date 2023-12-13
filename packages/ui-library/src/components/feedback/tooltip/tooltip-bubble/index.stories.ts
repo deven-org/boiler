56fc6ac133c2ebe3161af8299d59d0412b60dcfc
Manuel Seemann <manuel.seemann+github@gmail.com>
2023-12-13 11:56:45 +0100

    Feat/tooltip (#656)
    * feat(ui-library): add new tooltip component
    
    * feat(ui-library): update lock
    
    * fix(ui-library): compile
    
    * fix(ui-library): test, clean up
    
    * fix(tokens): renames elevations
    
    * fix(tokens): elevations use aliases
    
    * fix: Elevation tokens get generated.
    Adds Elevation token to tooltip
    
    * commit before pull
    
    * commit before pull
    
    * ignore package-lock for now
    
    * made it working as before but left the tries whe have done
    
    * better comment way :)
    
    * js -> ts
    
    * fix: token name in colors/SEM.json
    utilize Elevation token in tooltip
    
    * fix(ui-library): clean up
    
    * fix(ui-library): fix comments in literals
    
    ---------
    
    Co-authored-by: manuel.seemann <manuel.seemann@amadqg9ywp2v0.fritz.box>
    Co-authored-by: larserbach <larserbach@users.noreply.github.com>
    Co-authored-by: christian.b.hoffmann <christian.b.hoffmann@accenture.com>
    Co-authored-by: Christian Hoffmann <112889877+ChristianHoffmannS2@users.noreply.github.com>

---
+++ packages/ui-library/src/components/feedback/tooltip/tooltip-bubble/index.stories.ts
@@ -0,0 +1,28 @@
+import { html } from 'lit';
+import { BlrTooltipBubbleRenderFunction, BlrTooltipBubbleType } from '.';
+import { Themes } from '../../../../foundation/_tokens-generated/index.themes';
+
+export default {
+  title: 'Design System/Web Components/Feedback/Tooltip/TooltipBubble',
+  argTypes: {
+    theme: {
+      options: Themes,
+      control: { type: 'select' },
+    },
+  },
+};
+
+export const BlrTooltipBubble = (params: BlrTooltipBubbleType) => html`<div style="position: relative; height: 200px;">
+  ${BlrTooltipBubbleRenderFunction(params)}
+</div>`;
+
+BlrTooltipBubble.storyName = 'Tooltip Bubble';
+
+const args: BlrTooltipBubbleType = {
+  theme: 'Light',
+  message: 'Tooltip text comes here Tooltip text comes here',
+  visible: true,
+  static: true,
+};
+
+BlrTooltipBubble.args = args;
