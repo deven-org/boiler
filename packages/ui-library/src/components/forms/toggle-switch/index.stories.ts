8ab0852cd20414ec3a3802ef8209410f31391870
JpunktWpunkt <jessica.wittig@accenture.com>
2023-12-14 16:11:12 +0100

    feat(storybook): added argTableTypes

--- packages/ui-library/src/components/forms/toggle-switch/index.stories.ts
+++ packages/ui-library/src/components/forms/toggle-switch/index.stories.ts
@@ -8,16 +8,37 @@ export default {
   title: 'Design System/Web Components/Forms/ToggleSwitch',
   argTypes: {
     size: {
+      name: 'sizeVariant',
+      description: 'Choose size of the component.',
       options: FormSizes,
-      control: { type: 'select' },
+      control: { type: 'radio' },
+      table: {
+        category: 'Appearance',
+      },
+    },
+    hasHint: {
+      description: 'Choose if component has a hint message.',
+      options: { type: 'boolean' },
+      table: {
+        category: 'Content / Settings',
+      },
     },
     hintMessage: {
+      description: 'Enter string used used as hint message.',
       if: { arg: 'hasHint', eq: true },
+      table: {
+        category: 'Content / Settings',
+      },
     },
     hintIcon: {
+      name: 'hintMessageIcon',
+      description: 'Select an icon which is displayed in front of the hint message.',
       options: [undefined, ...PureIconKeys],
       control: { type: 'select' },
       if: { arg: 'hasHint', eq: true },
+      table: {
+        category: 'Content / Settings',
+      },
     },
     variant: {
       options: IconPositionVariant,
@@ -26,6 +47,36 @@ export default {
     theme: {
       options: Themes,
       control: { type: 'select' },
+      table: {
+        category: 'Appearance',
+      },
+    },
+    showStateLabel: {
+      description: 'Choose if component has a label.',
+      table: {
+        category: 'Content / Settings',
+      },
+    },
+    label: {
+      description: 'Enter string used as label text.',
+      table: {
+        category: 'Content / Settings',
+      },
+      if: { arg: 'showStateLabel', eq: true },
+    },
+    onLabel: {
+      description: 'Enter string used as on label text.',
+      table: {
+        category: 'Content / Settings',
+      },
+      if: { arg: 'showStateLabel', eq: true },
+    },
+    offLabel: {
+      description: 'Enter string used as off label text.',
+      table: {
+        category: 'Content / Settings',
+      },
+      if: { arg: 'showStateLabel', eq: true },
     },
     toggleOnIcon: {
       options: [undefined, ...PureIconKeys],
@@ -48,15 +99,15 @@ const logEventType = (event: Event) => {
 
 const args: BlrToggleSwitchType = {
   theme: 'Light',
+  size: 'md',
+  showStateLabel: true,
   label: 'Toggle Switch Option',
   onLabel: 'On',
   offLabel: 'Off',
-  showStateLabel: true,
   checkInputId: 'switch',
   disabled: false,
   readonly: false,
   checked: false,
-  size: 'md',
   hasHint: true,
   hintMessage: 'Field is used for hint',
   hintIcon: 'blrInfo',
