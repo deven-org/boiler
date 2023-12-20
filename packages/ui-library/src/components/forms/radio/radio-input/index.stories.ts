35df4c399d6dd961a30a296ac8a3ac86babfa917
kaikdi <51678690+kaikdi@users.noreply.github.com>
2023-12-20 17:26:15 +0100

    feat(ui-library): add figma plugin to storybook (#703)

--- packages/ui-library/src/components/forms/radio/radio-input/index.stories.ts
+++ packages/ui-library/src/components/forms/radio/radio-input/index.stories.ts
@@ -185,6 +185,10 @@ export default {
     },
   },
   parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/C4vgEKz8mKyulJ4gm3Qdql/%F0%9F%AB%A7-%5BBLR%5D-The-B01LER?node-id=3618%3A125199&mode=dev',
+    },
     viewMode: 'docs',
     layout: 'centered',
     docs: {
