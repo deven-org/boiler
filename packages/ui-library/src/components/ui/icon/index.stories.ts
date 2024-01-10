e16960cf5f1e72d70d68c46ccf05b0aa91e398f7
Barkley <barkley.taylor@accenture.com>
2024-01-10 14:21:22 +0100

    feat(ui): add icon docs

--- packages/ui-library/src/components/ui/icon/index.stories.ts
+++ packages/ui-library/src/components/ui/icon/index.stories.ts
@@ -1,6 +1,5 @@
 import { html } from 'lit-html';
 import { IconKeys } from '@boiler/icons';
-
 import { BlrIconType } from './index';
 import { BlrIconRenderFunction } from './renderFunction';
 
@@ -9,6 +8,22 @@ import { getIconName } from '../../../utils/get-icon-name';
 import { calculateIconName } from '../../../utils/calculate-icon-name';
 import { classMap } from 'lit/directives/class-map.js';
 import { SizesType } from '../../../globals/types';
+import { Themes } from '../../../foundation/_tokens-generated/index.themes';
+
+const sharedStyles = html`
+  <style>
+    .wrapper {
+      padding: 1.25em;
+    }
+    .label {
+      font-family: Source Sans Pro, sans-serif;
+      font-weight: 400;
+      line-height: 1rem;
+      font-size: 2rem;
+      text-align: center;
+    }
+  </style>
+`;
 
 // this loads the all components instances and registers their html tags
 import '../../../index';
@@ -18,10 +33,51 @@ export default {
   argTypes: {
     size: {
       options: Sizes,
+      description: 'Select size of the component.',
       control: { type: 'select' },
+      table: {
+        category: 'Appearance',
+      },
+    },
+    theme: {
+      options: Themes,
+      control: { type: 'select' },
+      table: {
+        category: 'Appearance',
+      },
+    },
+    icon: {
+      description: 'Select the icon of the component.',
+      table: {
+        category: 'Content / Settings',
+      },
+    },
+    ignoreSize: {
+      table: {
+        category: 'Content / Settings',
+      },
+    },
+    arialabel: {
+      name: 'ariaLabel',
+      description:
+        'Provides additional information about the elements purpose and functionality to assistive technologies, such as screen readers.',
+      table: {
+        category: 'Accessibility',
+      },
     },
   },
   parameters: {
+    layout: 'centered',
+    docs: {
+      description: {
+        component: `<Markdown>
+        An icon component typically displays a small, visually recognizable graphic or symbol that represents a particular function, object, or concept.
+- [**Appearance**](#appearance)
+ - [**Size Variant**](#size-variant) 
+</Markdown>
+        `,
+      },
+    },
     design: {
       type: 'figma',
       url: 'https://www.figma.com/file/C4vgEKz8mKyulJ4gm3Qdql/%F0%9F%AB%A7-%5BBLR%5D-The-B01LER?node-id=947%3A31105&mode=dev',
@@ -32,7 +88,7 @@ export default {
 
 const allIcons = getIconName(IconKeys);
 
-export const BlrIcon = (params: BlrIconType) => {
+export const Icon = (params: BlrIconType) => {
   const classes = classMap({
     'icon-gallery-layout': true,
   });
@@ -53,11 +109,44 @@ export const BlrIcon = (params: BlrIconType) => {
   </div>`;
 };
 
-BlrIcon.storyName = 'Icon';
-
-const args: BlrIconType = {
+const defaultParams: BlrIconType = {
+  theme: 'Light',
   size: 'lg',
+  icon: 'blr360Lg',
   ignoreSize: false,
 };
 
-BlrIcon.args = args;
+Icon.args = defaultParams;
+
+/**
+ * ## Appearance
+ * ### Size Variant
+ * The Checkbox component comes in 3 sizes: SM, MD and LG.
+ */
+export const SizeVariant = () => {
+  return html`
+    ${sharedStyles}
+    <div class="wrapper">
+      <div class="stories-checkbox">
+        <h3 class="label">Icon SM</h3>
+        ${Icon({
+          ...defaultParams,
+          size: 'sm',
+        })}
+        <h3 class="label">Icon MD</h3>
+        ${Icon({
+          ...defaultParams,
+          size: 'md',
+        })}
+        <h3 class="label">Icon LG</h3>
+        ${Icon({
+          ...defaultParams,
+          size: 'lg',
+        })}
+      </div>
+    </div>
+  `;
+};
+SizeVariant.story = {
+  name: ' ',
+};
