f7ad885b372e43c27a29ef4cb539717ce6e03a80
David Kennedy <127498135+davidken91@users.noreply.github.com>
2024-01-22 16:07:48 +0100

    Feature/513 tabbar use slots instead of arrays in the tabbar (#799)
    * replacing array with slots in tabbar component

--- packages/ui-library/src/components/navigation/tab-bar/renderFunction.ts
+++ packages/ui-library/src/components/navigation/tab-bar/renderFunction.ts
@@ -1,7 +1,8 @@
+import { TemplateResult } from 'lit-html';
 import { BlrTabBarType } from '.';
 import { genericBlrComponentRenderer } from '../../../utils/typesafe-generic-component-renderer';
 
 export const TAG_NAME = 'blr-tab-bar';
 
-export const BlrTabBarRenderFunction = (params: BlrTabBarType) =>
-  genericBlrComponentRenderer<BlrTabBarType>(TAG_NAME, { ...params });
+export const BlrTabBarRenderFunction = (params: BlrTabBarType, children?: TemplateResult<1>) =>
+  genericBlrComponentRenderer<BlrTabBarType>(TAG_NAME, { ...params }, children);
