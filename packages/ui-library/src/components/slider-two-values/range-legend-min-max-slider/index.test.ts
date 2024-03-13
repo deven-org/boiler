9363f945b642e029442f846ae354136d994898cd
Remmy Cat Stock <3317423+remmycat@users.noreply.github.com>
2024-03-13 10:20:57 +0100

    feat(ui-components): implement events for number input (#937)
    * feat(ui-components): implement events for number input
    ---------
    
    Co-authored-by: Christian Hoffmann <112889877+ChristianHoffmannS2@users.noreply.github.com>
    Co-authored-by: JpunktWpunkt <jessica.wittig@accenture.com>

--- packages/ui-library/src/components/slider-two-values/range-legend-min-max-slider/index.test.ts
+++ packages/ui-library/src/components/slider-two-values/range-legend-min-max-slider/index.test.ts
@@ -3,51 +3,48 @@ import '@boiler/ui-library/dist';
 import { BlrRangeLegendMinMaxSliderRenderFunction } from './renderFunction';
 import type { BlrRangeLegendMinMaxSliderType } from '.';
 
-import { expect, fixture } from "@open-wc/testing";
+import { expect, fixture } from '@open-wc/testing';
 import { querySelectorAllDeep, querySelectorDeep } from 'query-selector-shadow-dom';
 
 const sampleParams: BlrRangeLegendMinMaxSliderType = {
-    theme: 'Light',
-    rangeInputId: 'range-legend-cmpt',
-    startValue: '200$',
-    endValue: '400$',
-    size: 'lg',
-    list: [
-        '100$',
-        '200$',
-        '300$',
-        '400$',
-        '500$',
-    ],
-    stepFactor: 1,
-    btnVariant: 'primary',
-    incrementIcon: 'blrPlus',
-    decrementIcon: 'blrMinus',
-    onChange: () => null,
+  theme: 'Light',
+  rangeInputId: 'range-legend-cmpt',
+  startValue: '200$',
+  endValue: '400$',
+  size: 'lg',
+  list: ['100$', '200$', '300$', '400$', '500$'],
+  stepFactor: 1,
+  btnVariant: 'primary',
+  incrementIcon: 'blrPlus',
+  decrementIcon: 'blrMinus',
+  onChange: () => null,
 };
 
 describe('blr-range-legend-min-max-slider', () => {
-    it('is having two sliders containing the right className', async () => {
-        const element = await fixture(BlrRangeLegendMinMaxSliderRenderFunction(sampleParams));
-        const inputWrapper = querySelectorDeep('.range-wrapper', element.getRootNode() as HTMLElement);
-        const textareas = querySelectorAllDeep('input', inputWrapper?.getRootNode() as HTMLElement);
-        const classNameInputOne = textareas[0]?.className;
-        const classNameInputTwo = textareas[1]?.className;
-        
-       expect(classNameInputOne).to.contain('range'); 
-       expect(classNameInputTwo).to.contain('range'); 
-    });
+  it('is having two sliders containing the right className', async () => {
+    const element = await fixture(BlrRangeLegendMinMaxSliderRenderFunction(sampleParams));
+    const inputWrapper = querySelectorDeep('.range-wrapper', element.getRootNode() as HTMLElement);
+    const textareas = querySelectorAllDeep('input', inputWrapper?.getRootNode() as HTMLElement);
+    const classNameInputOne = textareas[0]?.className;
+    const classNameInputTwo = textareas[1]?.className;
 
-    it('properly renders legend', async () => {
-        const element = await fixture(BlrRangeLegendMinMaxSliderRenderFunction(sampleParams));
-        const inputWrapper = querySelectorDeep('.range-wrapper', element.getRootNode() as HTMLElement);
-        const rangeNumbersElement = querySelectorDeep('.range__numbers', inputWrapper?.getRootNode() as HTMLElement);
-        const allRangePoints = querySelectorAllDeep('.range__point', rangeNumbersElement?.getRootNode() as HTMLParagraphElement);
+    expect(classNameInputOne).to.contain('range');
+    expect(classNameInputTwo).to.contain('range');
+  });
 
-        allRangePoints.forEach((point, i) => {
-            expect(point).to.exist;
-            expect(point.textContent).to.equal(sampleParams.list[i]);
-        });
-        expect(allRangePoints.length).to.equal(sampleParams.list.length)
+  it('properly renders legend', async () => {
+    const element = await fixture(BlrRangeLegendMinMaxSliderRenderFunction(sampleParams));
+    const inputWrapper = querySelectorDeep('.range-wrapper', element.getRootNode() as HTMLElement);
+    const rangeNumbersElement = querySelectorDeep('.range__numbers', inputWrapper?.getRootNode() as HTMLElement);
+    const allRangePoints = querySelectorAllDeep(
+      '.range__point',
+      rangeNumbersElement?.getRootNode() as HTMLParagraphElement
+    );
+
+    allRangePoints.forEach((point, i) => {
+      expect(point).to.exist;
+      expect(point.textContent).to.equal(sampleParams.list[i]);
     });
-})
+    expect(allRangePoints.length).to.equal(sampleParams.list.length);
+  });
+});
