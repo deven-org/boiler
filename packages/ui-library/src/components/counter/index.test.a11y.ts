47e192b3f2543ea25736e7a7c9f2f5a88fdcc238
Julius Walther <julius.walther@sinnerschrader.com>
2024-03-28 13:29:53 +0100

    test(ui-library): add a11y tests

---
+++ packages/ui-library/src/components/counter/index.test.a11y.ts
@@ -0,0 +1,22 @@
+import '@boiler/ui-library/dist/';
+
+import { BlrCounterRenderFunction } from './renderFunction';
+import type { BlrCounterType } from '.';
+
+import { combineProperties } from '../../utils/test/combine-properties';
+import { runA11yCombinationTests } from '../../utils/test/run-combination-test';
+
+const propertyCombinations = combineProperties<BlrCounterType>({
+  maxValue: [0, 100],
+  sizeVariant: ['sm', 'md', 'lg', undefined],
+  theme: ['Dark', 'Light'],
+  value: [0, 50, 100],
+  variant: ['error', 'neutral', 'warn'],
+});
+
+describe('blr-counter', () => {
+  runA11yCombinationTests({
+    combinationsList: propertyCombinations,
+    componentRenderer: BlrCounterRenderFunction,
+  });
+});
