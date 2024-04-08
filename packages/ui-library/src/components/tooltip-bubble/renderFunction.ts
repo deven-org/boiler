3bd6ca4815a44bc303e6eedf831f9c07e9600a07
angsherpa456 <ang.sherpa@accenture.com>
2024-04-08 12:50:26 +0200

    fix(ui-library): rename tooltip component (#860)

--- packages/ui-library/src/components/tooltip-bubble/renderFunction.ts
+++
@@ -1,7 +0,0 @@
-import { BlrTooltipBubbleType } from '.';
-import { genericBlrComponentRenderer } from '../../utils/typesafe-generic-component-renderer';
-
-export const TAG_NAME = 'blr-tooltip-bubble';
-
-export const BlrTooltipBubbleRenderFunction = (params: BlrTooltipBubbleType) =>
-  genericBlrComponentRenderer<BlrTooltipBubbleType>(TAG_NAME, { ...params });
