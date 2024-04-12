5b11cb5892a18091f86406a7fe386ea77dbd8f90
Julius Walther <julius.walther@sinnerschrader.com>
2024-04-12 09:56:47 +0200

    feat(ui-library): introduce theme token iterators for css

--- packages/ui-library/src/foundation/_tokens-generated/index.pseudo.generated.ts
+++ packages/ui-library/src/foundation/_tokens-generated/index.pseudo.generated.ts
@@ -22,7 +22,9 @@ import {
 
 import { semanticTokens as semanticTokensDark, componentTokens as componentTokensDark } from './index.Dark.generated';
 
+/* this is the old theming approach we try to get refactored */
 import { CSSResult } from 'lit';
+import { makeIterator, typeSafeCss } from '../../utils/css-in-ts/make-token-part-iterator';
 
 export function renderThemedCssStrings(renderFunc: (cT: componentTokensType, sT: semanticTokensType) => CSSResult): {
   tokenizedLight: CSSResult;
@@ -49,3 +51,43 @@ export function renderThemedCssStrings(renderFunc: (cT: componentTokensType, sT:
     tokenizedDark,
   };
 }
+
+/* this will be used by the new approach */
+import { componentTokens as cmpLight } from './__component-tokens.Light.generated.mjs';
+import { componentTokens as cmpDark } from './__component-tokens.Dark.generated.mjs';
+import { semanticTokens as semLight } from './__semantic-tokens.Light.generated.mjs';
+import { semanticTokens as semDark } from './__semantic-tokens.Dark.generated.mjs';
+
+export const componentTokenTree = {
+  Light: cmpLight.cmp,
+  Dark: cmpDark.cmp,
+};
+
+export const semanticTokenTree = {
+  Light: semLight.sem,
+  Dark: semDark.sem,
+};
+
+export const ComponentThemeIterator = (
+  renderFunction: (
+    theme: keyof typeof componentTokenTree,
+    cmp: typeof componentTokenTree.Light,
+    css: typeof typeSafeCss
+  ) => string
+) => {
+  const it = makeIterator<typeof componentTokenTree>();
+
+  return it(componentTokenTree, renderFunction);
+};
+
+export const SemanticThemeIterator = (
+  renderFunction: (
+    theme: keyof typeof semanticTokenTree,
+    sem: typeof semanticTokenTree.Light,
+    css: typeof typeSafeCss
+  ) => string
+) => {
+  const it = makeIterator<typeof semanticTokenTree>();
+
+  return it(semanticTokenTree, renderFunction);
+};
