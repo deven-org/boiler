af7ba8b5f5179db894ea89f69c378b505efd2f39
Julius Walther <julius.walther@sinnerschrader.com>
2024-04-12 10:06:47 +0200

    refactor(ui-library): adapt range-min-max-slider

--- packages/ui-library/src/foundation/_tokens-generated/index.pseudo.generated.ts
+++ packages/ui-library/src/foundation/_tokens-generated/index.pseudo.generated.ts
@@ -11,48 +11,7 @@
 
   This will make it possible to switch prebuild themes on the fly on a component level
 */
-
-import { componentTokens as componentTokensType } from './componentTokensType.generated';
-import { semanticTokens as semanticTokensType } from './semanticTokensType.generated';
-
-import {
-  semanticTokens as semanticTokensLight,
-  componentTokens as componentTokensLight,
-} from './index.Light.generated';
-
-import { semanticTokens as semanticTokensDark, componentTokens as componentTokensDark } from './index.Dark.generated';
-
-/* this is the old theming approach we try to get refactored */
-import { CSSResult } from 'lit';
 import { makeIterator, typeSafeCss } from '../../utils/css-in-ts/make-token-part-iterator';
-
-export function renderThemedCssStrings(renderFunc: (cT: componentTokensType, sT: semanticTokensType) => CSSResult): {
-  tokenizedLight: CSSResult;
-  tokenizedDark: CSSResult;
-} {
-  let tokenizedLight, tokenizedDark;
-
-  try {
-    tokenizedLight = renderFunc(componentTokensLight, semanticTokensLight);
-  } catch (error) {
-    console.error(error);
-  }
-
-  try {
-    tokenizedDark = renderFunc(componentTokensDark, semanticTokensDark);
-  } catch (error) {
-    console.error(error);
-  }
-
-  return {
-    // @ts-expect-error todo
-    tokenizedLight,
-    // @ts-expect-error todo
-    tokenizedDark,
-  };
-}
-
-/* this will be used by the new approach */
 import { componentTokens as cmpLight } from './__component-tokens.Light.generated.mjs';
 import { componentTokens as cmpDark } from './__component-tokens.Dark.generated.mjs';
 import { semanticTokens as semLight } from './__semantic-tokens.Light.generated.mjs';
