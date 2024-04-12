85d66eb0aa15373a187fafc012c0eba0732e6700
Julius Walther <julius.walther@sinnerschrader.com>
2024-04-12 10:04:07 +0200

    refactor(ui-library): adapt form-label-inline

--- packages/ui-library/src/components/form-label/form-label-inline/index.ts
+++ packages/ui-library/src/components/form-label/form-label-inline/index.ts
@@ -1,7 +1,9 @@
+import { ThemeType } from '../../../foundation/_tokens-generated/index.themes';
 import { InputSizesType } from '../../../globals/types';
 
 export type BlrFormLabelInlineType = {
   labelText: string;
   labelSize: InputSizesType;
   forValue: string;
+  theme: ThemeType;
 };
