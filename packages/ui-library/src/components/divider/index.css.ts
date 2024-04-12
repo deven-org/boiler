49c2870663f408cf7b6c4cdf2872ad2cdfc1bac2
Julius Walther <julius.walther@sinnerschrader.com>
2024-04-12 10:03:13 +0200

    refactor(ui-library): adapt divider

--- packages/ui-library/src/components/divider/index.css.ts
+++ packages/ui-library/src/components/divider/index.css.ts
@@ -1,22 +1,24 @@
-import { renderThemedCssStrings } from "../../foundation/_tokens-generated/index.pseudo.generated";
-import { typeSafeNestedCss } from "../../utils/nested-typesafe-css-literals";
+import { ComponentThemeIterator } from "../../foundation/_tokens-generated/index.pseudo.generated";
+import { typeSafeNestedCss } from "../../utils/css-in-ts/nested-typesafe-css-literals";
 
-export const { tokenizedLight: dividerLight, tokenizedDark: dividerDark } = renderThemedCssStrings((componentTokens) => {
-  const { Divider } = componentTokens.cmp;
+export const staticStyles = typeSafeNestedCss`
+  ${ComponentThemeIterator((theme, cmp, typeSafeCss) => {
+    const { Divider } = cmp;
 
-  return typeSafeNestedCss`
-    .blr-divider {
-      background-color: ${Divider.Container.BackgroundColor};
+    return typeSafeCss/*css*/ `
+      .blr-divider.${theme} {
+        background-color: ${Divider.Container.BackgroundColor};
 
-      &.vertical {
-        width: ${Divider.Container.Vertical.Width};
-        height: 100%;
+        &.vertical {
+          width: ${Divider.Container.Vertical.Width};
+          height: 100%;
+        }
+
+        &.horizontal {
+          width: 100%;
+          height: ${Divider.Container.Horizontal.Height};
+        }
       }
-
-      &.horizontal {
-        width: 100%;
-        height: ${Divider.Container.Horizontal.Height};
-      }
-    }
-  `;
-});
+    `;
+  })}
+`;
