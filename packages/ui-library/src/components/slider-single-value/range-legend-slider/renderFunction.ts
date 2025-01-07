7c4de52a4837608e93916d1094d4f3672f4c5a72
larserbach <larserbach@users.noreply.github.com>
2025-01-07 15:25:31 +0100

    fix: removes range slider files, tokens and references

--- packages/ui-library/src/components/slider-single-value/range-legend-slider/renderFunction.ts
+++
@@ -1,7 +0,0 @@
-import { BlrRangeLegendSliderType } from './index.js';
-import { genericBlrComponentRenderer } from '../../../utils/typesafe-generic-component-renderer.js';
-
-export const TAG_NAME = 'blr-range-legend-slider';
-
-export const BlrRangeLegendSliderRenderFunction = (params: BlrRangeLegendSliderType) =>
-  genericBlrComponentRenderer<BlrRangeLegendSliderType>(TAG_NAME, { ...params });
